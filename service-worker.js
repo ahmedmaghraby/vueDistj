85158e661228ef5b67587c50447ae911545ec667
ahmed maghraby <38741761+ahmedmaghraby@users.noreply.github.com>
2020-12-19 13:05:07 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/precache-manifest.cf42de98da1ac4f1f5355a3151d5ab32.js", "/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate(0),
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://reqres.in'),
+  workbox.strategies.networkFirst(),
+)
+
